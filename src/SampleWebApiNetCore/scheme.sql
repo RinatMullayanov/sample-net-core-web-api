90a67d4ae7a7271a1a414a3f3378f23b526c7085
MullayanovRR <MullayanovRR@ufa.rn-inform.ru>
2016-11-30 21:21:05 +0500

    add db scheme sql script

---
+++ src/SampleWebApiNetCore/scheme.sql
@@ -0,0 +1,14 @@
+﻿--PostgreSQL
+CREATE TABLE public.wells
+(
+    id integer,
+    name character varying(256) NOT NULL,
+    PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.wells
+    OWNER to test;
