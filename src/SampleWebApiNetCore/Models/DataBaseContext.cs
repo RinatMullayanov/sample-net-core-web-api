ae448db91a694c893c8fb3a972c85609b9e4513b
MullayanovRR <MullayanovRR@ufa.rn-inform.ru>
2016-11-30 11:46:46 +0500

    begin to add PostgreSQL

---
+++ src/SampleWebApiNetCore/Models/DataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApiNetCore.Models
+{
+    public class DataBaseContext: DbContext
+    {
+        public DbSet<Well> Wells { get; set; }
+        public DataBaseContext(DbContextOptions<DataBaseContext> options): base(options)
+        {
+        }
+    }
+}
